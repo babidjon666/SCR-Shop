68e646d835098aa1cc398a83f26e1fe816f2023a
MarkPolyakov <markpolya@icloud.com>
2023-10-23 22:53:52 +0500

    Initial Commit

---
+++ SCR Shop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SCR Shop
+//
+//  Created by Марк Поляков on 23.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SCR Shop/SCR_ShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  SCR_ShopApp.swift
+//  SCR Shop
+//
+//  Created by Марк Поляков on 23.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct SCR_ShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
